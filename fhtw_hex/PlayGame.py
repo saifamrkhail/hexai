de23da426825a6e39b4788ba8332a1f7c092c585
Adrian Dimaano <ai22m033@technikum-wien.at>
2023-06-06 16:45:01 +0200

    schnelle Statistik-Generierung

--- fhtw_hex/PlayGame.py
+++ fhtw_hex/PlayGame.py
@@ -7,6 +7,3 @@ game = hex_engine.hexPosition(size=BOARD_SIZE)
 
 #play against the machine
 game.human_vs_machine(human_player=1, machine=AI.machine)
-
-#let machine play against random
-#game.machine_vs_machine(machine1=AI.machine, machine2=None)
